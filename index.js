24144abe866dcc7ae84a79c2b52c1c56675da9e8
Arthur Meireles <arthur_meireles@hotmail.com.br>
2020-09-08 08:59:43 -0300

    update

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 //IMPORTS
 const express = require('express');
 const app = express();
-const porta = 3000;
+const porta = 3333;
 const bodyParser = require('body-parser');
 const multer = require('multer');
 const connection = require('./database/database');
 const Aluno = require('./database/Aluno')
 const uploadsPath = 'uploads/';
-const path = require('path')
+const path = require('path');
 
 //Conexão
 connection.authenticate().then(() => {
-    console.log('conexao estabelecida.')
+  console.log('conexao estabelecida.')
 }).catch((msgError) => {
-    console.log(msgError);
+  console.log(msgError);
 });
 
 //Usando EJS e pasta com arquivos estáticos
@@ -22,22 +22,22 @@ app.use(express.static('public'));
 
 //Traduz form para js
 app.use(bodyParser.urlencoded({
-    extended: false
+  extended: false
 }));
 app.use(bodyParser.json());
 
 //Configurando Multer
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, uploadsPath);
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + path.extname(file.originalname));
-    }
+  destination: (req, file, cb) => {
+    cb(null, uploadsPath);
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  }
 });
 
 const upload = multer({
-    storage
+  storage
 });
 
 
@@ -45,75 +45,75 @@ const upload = multer({
 
 //Homepage
 app.get('/', (req, res) => {
-    res.render('index');
+  res.render('index');
 });
 
 
 //Cadastro de horas
 app.get('/cadastrar', (req, res) => {
-    res.render('cadastrar');
+  res.render('cadastrar');
 });
 
 app.post('/cadastrar', upload.single('file'), (req, res) => {
-    var nome = req.body.nome;
-    var matricula = req.body.matricula;
-    var curso = req.body.curso;
-    var turma = req.body.turma;
-    var horas = req.body.horas;
-    var file = req.file.path;
-    Aluno.create({
-        nome,
-        matricula,
-        curso,
-        turma,
-        horas,
-        file,
-    }).then(() => {
-        res.redirect('/pesquisar');
-    });
+  var nome = req.body.nome;
+  var matricula = req.body.matricula;
+  var curso = req.body.curso;
+  var turma = req.body.turma;
+  var horas = req.body.horas;
+  var file = req.file.path;
+  Aluno.create({
+    nome,
+    matricula,
+    curso,
+    turma,
+    horas,
+    file,
+  }).then(() => {
+    res.redirect('/pesquisar');
+  });
 
 });
 
 //Pesquisar
 app.get('/pesquisar', (req, res) => {
-    Aluno.findAll({
-        raw: true,
-        order: [
-            ['matricula', 'ASC'],
-        ]
-    }).then(alunos => {
-        res.render('pesquisar', {
-            alunos
-        })
-    });
+  Aluno.findAll({
+    raw: true,
+    order: [
+      ['matricula', 'ASC'],
+    ]
+  }).then(alunos => {
+    res.render('pesquisar', {
+      alunos
+    })
+  });
 });
 
 app.get('/resultados', async (req, res) => {
-    var pesquisa = req.query.pesquisa;
-    await Aluno.findAll({
-        raw: true,
-        where: {
-            matricula: pesquisa
-        },
-    }).then(resultados => {
-        res.render('resultados', {
-            resultados
-        })
-    });
+  var pesquisa = req.query.pesquisa;
+  await Aluno.findAll({
+    raw: true,
+    where: {
+      matricula: pesquisa
+    },
+  }).then(resultados => {
+    res.render('resultados', {
+      resultados
+    })
+  });
 });
 
 //Baixar arquivo
 app.get('/baixar', (req, res) => {
-    url = req.query.path;
-    res.download(url);
+  url = req.query.path;
+  res.download(url);
 });
 
 //Rotas Invalidas
 app.get('*', (req, res) => {
-    res.render('404');
+  res.render('404');
 })
 
 //SERVIDOR
 app.listen(porta, () => {
-    console.log('Servidor on na porta:' + porta);
+  console.log('Servidor on na porta:' + porta);
 });
